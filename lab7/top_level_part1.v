25f8248afcd8f8945511b8e48e403ae411cc0862
samarqureshii <samarq713@gmail.com>
2023-11-09 16:28:39 -0500

    add hex deocer

--- lab7/top_level_part1.v
+++ lab7/top_level_part1.v
@@ -1,10 +1,39 @@
-//top level
-
 module top_level_part1(KEY, SW, HEX0);
-	input [9:0] SW;
-	input KEY;
-	output [3:0] HEX0;
-	
-	part1 p1(.address(SW[8:4]), .clock(KEY), .data(SW[3:0]), .wren(SW[9]), .q(HEX0));
+    input [9:0] SW;
+    input KEY;
+    output [6:0] HEX0; 
+    
+    wire [3:0] q;
+    
+    part1 p1(
+        .address(SW[8:4]),
+        .clock(KEY),
+        .data(SW[3:0]),
+        .wren(SW[9]),
+        .q(q)  
+    );
+    
+    hex_decoder hd(
+        .c(q),          //data out
+        .display(HEX0)  
+    );
+
 endmodule
 
+module hex_decoder(c, display);
+    input [3:0] c;
+    output [6:0] display;
+    
+    assign c0 = c[0];
+    assign c1 = c[1];
+    assign c2 = c[2];
+    assign c3 = c[3];
+
+    assign display[0] = (~c3 & ~c2 & ~c1 & c0) + (~c3 & c2 & ~c1 & ~c0) + (c3 & ~c2 & c1 & c0) + (c3 & c2 & ~c1 & c0);
+    assign display[1] = (~c3 & c2 & ~c1 & c0) + (~c3 & c2 & c1 & ~c0) + (c3 & ~c2 & c1 & c0) + (c3 & c2 & ~c1 & ~c0) + (c3 & c2 & c1 & ~c0) + (c3 & c2 & c1 & c0);
+    assign display[2] = (~c3 & ~c2 & c1 & ~c0) + (c3 & c2 & ~c1 & ~c0) + (c3 & c2 & c1 & ~c0) + (c3 & c2 & c1 & c0);	
+    assign display[3] = (~c3 & ~c2 & ~c1 & c0) + (~c3 & c2 & ~c1 & ~c0) + (~c3 & c2 & c1 & c0) + (c3 & ~c2 & ~c1 & c0) + (c3 & ~c2 & c1 & ~c0) + (c3 & c2 & c1 & c0);
+    assign display[4] = (~c3 & ~c2 & ~c1 & c0) + (~c3 & ~c2 & c1 & c0) + (~c3 & c2 & ~c1 & ~c0) + (~c3 & c2 & ~c1 & c0) + (~c3 & c2 & c1 & c0) + (c3 & ~c2 & ~c1 & c0);
+    assign display[5] = (~c3 & ~c2 & ~c1 & c0) + (~c3 & ~c2 & c1 & ~c0) + (~c3 & ~c2 & c1 & c0) + (~c3 & c2 & c1 & c0) + (c3 & c2 & ~c1 & c0);
+    assign display[6] = (~c3 & ~c2 & ~c1 & ~c0) + (~c3 & ~c2 & ~c1 & c0) + (~c3 & c2 & c1 & c0) + (c3 & c2 & ~c1 & ~c0);
+endmodule;
